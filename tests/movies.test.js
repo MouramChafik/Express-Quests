e9071a99471ad2889d90409f6ebd417f680b040a
mouram chafikE <mrmouramito@gmail.com>
2023-11-28 12:05:49 +0100

    Put test ok for movies and users

--- tests/movies.test.js
+++ tests/movies.test.js
@@ -83,3 +83,89 @@ describe("POST /api/movies", () => {
     expect(response.status).toEqual(500);
   });
 });
+
+//test la route PUT
+
+describe("PUT /api/movies/:id", () => {
+  it("should edit a movie", async () => {
+    const newMovie = {
+      title: "Avatar",
+      director: "James Cameron",
+      year: "2010",
+      color: "1",
+      duration: 162,
+    };
+
+    const [insertResult] = await database.query(
+      "INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)",
+      [
+        newMovie.title,
+        newMovie.director,
+        newMovie.year,
+        newMovie.color,
+        newMovie.duration,
+      ]
+    );
+
+    const id = insertResult.insertId;
+
+    const updatedMovie = {
+      title: "Wild is Life",
+      director: "Alan Smithee",
+      year: "2023",
+      color: "0",
+      duration: 120,
+    };
+
+    const response = await request(app)
+      .put(`/api/movies/${id}`)
+      .send(updatedMovie);
+
+    expect(response.status).toEqual(204);
+
+    const [selectResult] = await database.query("SELECT * FROM movies WHERE id=?", [id]);
+
+    const [movieInDatabase] = selectResult;
+
+    expect(movieInDatabase).toHaveProperty("id");
+
+    expect(movieInDatabase).toHaveProperty("title");
+    expect(movieInDatabase.title).toEqual(updatedMovie.title);
+
+    expect(movieInDatabase).toHaveProperty("director");
+    expect(movieInDatabase.director).toEqual(updatedMovie.director);
+
+    expect(movieInDatabase).toHaveProperty("year");
+    expect(movieInDatabase.year).toEqual(updatedMovie.year);
+
+    expect(movieInDatabase).toHaveProperty("color");
+    expect(movieInDatabase.color).toEqual(updatedMovie.color);
+
+    expect(movieInDatabase).toHaveProperty("duration");
+    expect(movieInDatabase.duration).toEqual(updatedMovie.duration);
+  });
+
+  it("should return an error if required properties are missing", async () => {
+    const movieWithMissingProps = { title: "Harry Potter" };
+
+    const response = await request(app)
+      .put(`/api/movies/1`)
+      .send(movieWithMissingProps);
+
+    expect(response.status).toEqual(500);
+  });
+
+  it("should return a 404 if trying to update a non-existent movie", async () => {
+    const newMovie = {
+      title: "Avatar",
+      director: "James Cameron",
+      year: "2009",
+      color: "1",
+      duration: 162,
+    };
+
+    const response = await request(app).put("/api/movies/0").send(newMovie);
+
+    expect(response.status).toEqual(404);
+  });
+});
--- tests/users.test.js
+++ tests/users.test.js
@@ -86,3 +86,90 @@ describe("POST /api/users", () => {
     expect(response.status).toEqual(500);
   });
 });
+
+
+//test la route PUT
+
+describe("PUT /api/users/:id", () => {
+  it("should edit a user", async () => {
+    const newUser = {
+      firstname: "David",
+      lastname: "Cameron",
+      email: "exemple@wild.com",
+      city: "Lyon",
+      language: "french",
+    };
+
+    const [insertResult] = await database.query(
+      "INSERT INTO users (firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
+      [
+        newUser.firstname,
+        newUser.lastname,
+        newUser.email,
+        newUser.city,
+        newUser.language,
+      ]
+    );
+
+    const id = insertResult.insertId;
+
+    const updatedUser = {
+      firstname: "Luca",
+      lastname: "Updated",
+      email: "updated@wild.me",
+      city: "Nice",
+      language: "Spanish",
+    };
+
+    const response = await request(app)
+      .put(`/api/users/${id}`)
+      .send(updatedUser);
+
+    expect(response.status).toEqual(204);
+
+    const [selectResult] = await database.query("SELECT * FROM users WHERE id=?", [id]);
+
+    const [userInDatabase] = selectResult;
+
+    expect(userInDatabase).toHaveProperty("id");
+
+    expect(userInDatabase).toHaveProperty("firstname");
+    expect(userInDatabase.firstname).toEqual(updatedUser.firstname);
+
+    expect(userInDatabase).toHaveProperty("lastname");
+    expect(userInDatabase.lastname).toEqual(updatedUser.lastname);
+
+    expect(userInDatabase).toHaveProperty("email");
+    expect(userInDatabase.email).toEqual(updatedUser.email);
+
+    expect(userInDatabase).toHaveProperty("city");
+    expect(userInDatabase.city).toEqual(updatedUser.city);
+
+    expect(userInDatabase).toHaveProperty("language");
+    expect(userInDatabase.language).toEqual(updatedUser.language);
+  });
+
+  it("should return an error if required properties are missing", async () => {
+    const userWithMissingProps = { firstname: "Harry Potter" };
+
+    const response = await request(app)
+      .put(`/api/users/1`)
+      .send(userWithMissingProps);
+
+    expect(response.status).toEqual(500);
+  });
+
+  it("should return a 404 if trying to update a non-existent user", async () => {
+    const newUser = {
+      firstname: "Avatar",
+      lastname: "James Cameron",
+      email: "2009",
+      city: "1",
+      language: 162,
+    };
+
+    const response = await request(app).put("/api/movies/0").send(newUser);
+
+    expect(response.status).toEqual(404);
+  });
+});
